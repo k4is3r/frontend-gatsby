f194f902937f9c178f0ca7da8f0808764d2d946e
Eduardo Imery <ejwimery@gmail.com>
2019-08-29 15:50:51 -0600

    fixing links to projects live view

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,7 @@ import portfolio4 from "../images/portfolio4.png"
 import portfolio5 from "../images/portfolio5.png"
 import portfolio6 from "../images/portfolio6.png"
 import portfolio7 from "../images/portfolio7.png"
+import portfolio8 from "../images/portfolio8.png"
 
 export default class Projects extends Component {
   constructor(props) {
@@ -19,8 +20,8 @@ export default class Projects extends Component {
           imageAlt: "Sihuapilapa Resort",
           cardTitle: "Sihuapilapa Resort",
           cardSubtitle: "Hotel Resort page",
-          cardText: ["React", "Node", "Bootsrtrap"],
-          githubLink: "https://github.com/k4is3r",
+          cardText: ["React", "Node", "Bootsrtrap","Contentful"],
+          githubLink: "https://github.com/k4is3r/react-sectionTry",
           demoLink: "https://sihuapilaparesort.netlify.com/",
         },
         {
@@ -30,7 +31,7 @@ export default class Projects extends Component {
           cardTitle: "TodoList",
           cardSubtitle: "App",
           cardText: ["React", "Javascript"],
-          githubLink: "https://github.com/k4is3r",
+          githubLink: "https://github.com/k4is3r/todolist_react",
           demoLink: "https://silly-clarke-5e1c50.netlify.com/",
         },
         {
@@ -50,7 +51,7 @@ export default class Projects extends Component {
           cardTitle: "Car List",
           cardSubtitle: "App",
           cardText: ["Html5", "Css3", "Ruby On Rails"],
-          githubLink: "https://github.com/k4is3r",
+          githubLink: "https://github.com/k4is3r/my_frist_rails_app",
           demoLink: "https://enigmatic-garden-60350.herokuapp.com/",
         },
         {
@@ -60,18 +61,8 @@ export default class Projects extends Component {
           cardTitle: "Juego de Colores",
           cardSubtitle: "Game",
           cardText: ["Javascript", "CSS3", "Htlm5"],
-          githubLink: "https://github.com/k4is3r",
+          githubLink: "https://github.com/k4is3r/juegoColores",
           demoLink: "https://www.iwdevelop.com/projects/production2/",
-        },
-        {
-          id: 6,
-          cardImage: portfolio6,
-          imageAlt: "Sihuapilapa Resort",
-          cardTitle: "Sihuapilapa Resort",
-          cardSubtitle: "Hotel Resort page",
-          cardText: ["React", "Node", "Bootsrtrap"],
-          githubLink: "https://github.com/k4is3r",
-          demoLink: "https://sihuapilaparesort.netlify.com/",
         },
         {
           id: 6,
@@ -96,13 +87,13 @@ export default class Projects extends Component {
         {
           id: 8,
           cardImage: portfolio8,
-          imageAlt: "ninatex",
-          cardTitle: "Industrias Ninatex",
-          cardSubtitle: "",
-          cardText: ["React", "Node", "Bootsrtrap"],
-          githubLink: "http://www.industriasninatex.com/",
-          demoLink: "http://www.industriasninatex.com/",
-        }
+          imageAlt: "instaclone",
+          cardTitle: "StalkerGram",
+          cardSubtitle: "Instagram clone",
+          cardText: ["Python", "Django"],
+          githubLink: "https://github.com/k4is3r/stalkergram-django",
+          demoLink: "http://stalkergram.iwdevelop.com",
+        },
       ],
     }
   }
